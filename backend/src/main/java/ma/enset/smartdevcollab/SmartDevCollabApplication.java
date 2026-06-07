c152a45c4dfa45c781ebe932577a1b168bc129a7
AYA <110476082+Agrigah@users.noreply.github.com>
2026-06-07 21:00:47 +0100

    Add files via upload

---
+++ backend/src/main/java/ma/enset/smartdevcollab/SmartDevCollabApplication.java
@@ -0,0 +1,4 @@
+package ma.enset.smartdevcollab;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SmartDevCollabApplication { public static void main(String[] args){ SpringApplication.run(SmartDevCollabApplication.class,args);} }
